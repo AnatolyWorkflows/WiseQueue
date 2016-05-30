f0bcbbcc29090b9ff58422c74134efcc7ccb5d85
Anatoly <wiseworkflows@gmail.com>
2016-05-30 21:51:36 +1000

    Add base class that will be used for all objects which should have logger.

---
+++ WiseQueue.Core.Common/Models/BaseLoggerObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using WiseQueue.Core.Common.Logging;
+
+namespace WiseQueue.Core.Common.Models
+{
+    /// <summary>
+    /// Base class for objects that should have logger.
+    /// </summary>
+    public abstract class BaseLoggerObject
+    {
+        /// <summary>
+        /// The <see cref="IWiseQueueLogger"/> instance.
+        /// </summary>
+        protected readonly IWiseQueueLogger logger;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="loggerFactory">The <see cref="IWiseQueueLoggerFactory"/> instance.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="loggerFactory"/> is <see langword="null" />.</exception>
+        protected BaseLoggerObject(IWiseQueueLoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+                throw new ArgumentNullException("loggerFactory");
+
+            logger = loggerFactory.Create(GetType());
+        }
+    }
+}
