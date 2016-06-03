6736d53c9d58a049717c49d96ba61da11e2fc526
Anatoly <wiseworkflows@gmail.com>
2016-06-03 20:54:05 +1000

    Added interface for tasks' data context.

---
+++ WiseQueue.Core.Common/DataContexts/ITaskDataContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiseQueue.Core.Common.Entities;
+
+namespace WiseQueue.Core.Common.DataContexts
+{
+    /// <summary>
+    /// Interface shows that <c>object</c> is a task data context.
+    /// </summary>
+    public interface ITaskDataContext
+    {
+        /// <summary>
+        /// Insert a task into the storage.
+        /// </summary>
+        /// <param name="taskEntity">The <see cref="TaskEntity"/> instance.</param>
+        /// <returns>The task's identifier.</returns>
+        Int64 InsertTask(TaskEntity taskEntity);
+
+        /// <summary>
+        /// Get available task from the storage.
+        /// </summary>
+        /// <returns>The <see cref="TaskEntity"/> instance if it has been found. Otherwise, null.</returns>
+        TaskEntity GetAvailableTask();
+    }
+}
