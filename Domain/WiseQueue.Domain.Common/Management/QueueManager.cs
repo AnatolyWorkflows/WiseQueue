e4792dabce5d2e88aa655482d037cbbfe2ede4ee
Anatoly <wiseworkflows@gmail.com>
2016-06-09 21:58:54 +1000

    Added queue converter and queue data context into the queue manager as parameters.

--- Domain/WiseQueue.Domain.Common/Management/QueueManager.cs
+++ Domain/WiseQueue.Domain.Common/Management/QueueManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using WiseQueue.Core.Common.Converters.EntityModelConverters;
+using WiseQueue.Core.Common.DataContexts;
 using WiseQueue.Core.Common.Entities;
 using WiseQueue.Core.Common.Logging;
 using WiseQueue.Core.Common.Management;
@@ -9,14 +11,37 @@ namespace WiseQueue.Domain.Common.Management
 {
     public class QueueManager : BaseLoggerObject, IQueueManager
     {
+        #region Fields...
+
+        /// <summary>
+        /// The <see cref="IQueueConverter"/> instance.
+        /// </summary>
+        private readonly IQueueConverter queueConverter;
+
+        /// <summary>
+        /// The <see cref="IQueueDataContext"/> instance.
+        /// </summary>
+        private readonly IQueueDataContext queueDataContext;
+
+        #endregion 
+
         /// <summary>
         /// Constructor.
         /// </summary>
-        /// <param name="loggerFactory">The <see cref="IWiseQueueLoggerFactory"/> instance.</param>
+        /// <param name="queueConverter">The <see cref="IQueueConverter"/> instance.</param>
+        /// <param name="queueDataContext">The <see cref="IQueueDataContext"/> instance.</param>
+        /// <param name="loggerFactory">The <see cref="IWiseQueueLoggerFactory"/> instance.</param>        
         /// <exception cref="ArgumentNullException"><paramref name="loggerFactory"/> is <see langword="null" />.</exception>
-        public QueueManager(IWiseQueueLoggerFactory loggerFactory)
+        public QueueManager(IQueueConverter queueConverter, IQueueDataContext queueDataContext, IWiseQueueLoggerFactory loggerFactory)
             : base(loggerFactory)
         {
+            if (queueConverter == null) 
+                throw new ArgumentNullException("queueConverter");
+            if (queueDataContext == null) 
+                throw new ArgumentNullException("queueDataContext");
+
+            this.queueConverter = queueConverter;
+            this.queueDataContext = queueDataContext;
         }
 
         #region Implementation of IQueueManager
