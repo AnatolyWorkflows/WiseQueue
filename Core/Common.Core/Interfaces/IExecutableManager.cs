8af219fb4813c32f568989eae3b4d1ae4f9f3d30
Anatoly <wiseworkflows@gmail.com>
2016-07-15 22:26:57 +1000

    Added two interfaces in the Common.Core that will be used in whole system.

---
+++ Core/Common.Core/Interfaces/IExecutableManager.cs
@@ -0,0 +1,13 @@
+﻿namespace Common.Core.Interfaces
+{
+    /// <summary>
+    /// Interface shows that object has a method that can be used for executing its logic.
+    /// </summary>
+    public interface IExecutable
+    {
+        /// <summary>
+        /// Occurs when object should do its action.
+        /// </summary>
+        void Execute();
+    }
+}
---
+++ Core/Common.Core/Interfaces/IStartStoppable.cs
@@ -0,0 +1,18 @@
+﻿namespace Common.Core.Interfaces
+{
+    /// <summary>
+    /// Interface shows that object should be started before using and should be stopped after it.
+    /// </summary>
+    public interface IStartStoppable
+    {
+        /// <summary>
+        /// Start manager.
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Stop.
+        /// </summary>
+        void Stop(); 
+    }
+}
